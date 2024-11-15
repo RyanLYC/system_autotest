2de1576fde7ad9ad58d2652481743bb67b43acf0
liangyancheng <656040874@qq.com>
2024-11-16 00:25:25 +0800

    自动下载对应的浏览器驱动

--- common/tools.py
+++ common/tools.py
@@ -1,7 +1,10 @@
 import os
 
-# 获取项目绝对路径
 def get_project_path():
+    """
+    获取项目绝对路径
+    :return:
+    """
     # 项目名称 即是 文件夹的名称
     project_name = "system_autotest"
     # 获取当前的文件所在的文件夹路径 D:\PycharmProjects\py3.11.5\system_autotest\common
@@ -9,10 +12,15 @@ def get_project_path():
     # 截取项目路径 结果为 D:\PycharmProjects\py3.11.5\system_autotest
     return file_path[:file_path.find(project_name) + len(project_name)]
 
-# 拼接文件或者文件夹的路径，最后得到的路径前面或者后面增加/,因为windows和mac以及linux的路径分隔符不一样的，所以不能写死的，windows是 \ linux和mac是 /
-# sep(["config", "environment.yaml"], True)
-# 输出 \config\environment.yaml
+
 def sep(path, add_sep_before=False, add_sep_after=False):
+    """
+    拼接文件或者文件夹的路径，最后得到的路径前面或者后面增加/,因为windows和mac以及linux的路径分隔符不一样的，所以不能写死的，windows是 \ linux和mac是 /
+    :param path: ["config", "environment.yaml"]
+    :param add_sep_before: True
+    :param add_sep_after:
+    :return: \config\environment.yaml
+    """
     # 把数组合并成路径 ["config", "environment.yaml"] => config\environment.yaml
     all_path = os.sep.join(path)
     # 前面增加路径分隔符
---
+++ config/driver_config.py
@@ -0,0 +1,49 @@
+from time import sleep
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.driver_cache import DriverCacheManager
+
+
+# chrome driver 地址： https://registry.npmmirror.com/binary.html?path=chromedriver
+
+class DriverConfig:
+    def driver_config(self):
+        """
+        浏览器驱动
+        :return:
+        """
+        options = webdriver.ChromeOptions()
+        # 设置窗口大小，设置为1920*1080
+        options.add_argument("window-size=1920,1080")
+        # 去除"chrome正受到自动测试软件的控制"的提示
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # 解决selenium无法访问https的问题
+        options.add_argument("--ignore-certificate-errors")
+        # 允许忽略localhost上的TLS/SSL错误
+        options.add_argument("--allow-insecure-localhost")
+        # 设置为无痕模式
+        options.add_argument("--incognito")
+        # 设置为无头模式
+        # options.add_argument("--headless")
+        # 解决卡顿
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        # 自动下载对应的浏览器驱动
+        driver = webdriver.Chrome(ChromeDriverManager(url="https://registry.npmmirror.com/-/binary/chromedriver",
+                                                      latest_release_url="https://registry.npmmirror.com/-/binary/chromedriver/LATEST_RELEASE",
+                                                      cache_manager=DriverCacheManager(valid_range=365)).install(),
+                                  options=options)
+        # 删除所有cookies
+        driver.delete_all_cookies()
+
+        # driver.get("https://www.baidu.com/")
+        # sleep(3)
+        # driver.quit()
+
+        return driver
+
+
+if __name__ == '__main__':
+    DriverConfig().driver_config()
