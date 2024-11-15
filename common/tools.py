84716ff4f010e0d8150673820fe24f2cd60141ae
liangyancheng <656040874@qq.com>
2024-11-15 09:53:31 +0800

    读取yaml文件

---
+++ common/tools.py
@@ -0,0 +1,28 @@
+import os
+
+# 获取项目绝对路径
+def get_project_path():
+    # 项目名称 即是 文件夹的名称
+    project_name = "system_autotest"
+    # 获取当前的文件所在的文件夹路径 D:\PycharmProjects\py3.11.5\system_autotest\common
+    file_path = os.path.dirname(__file__)
+    # 截取项目路径 结果为 D:\PycharmProjects\py3.11.5\system_autotest
+    return file_path[:file_path.find(project_name) + len(project_name)]
+
+# 拼接文件或者文件夹的路径，最后得到的路径前面或者后面增加/,因为windows和mac以及linux的路径分隔符不一样的，所以不能写死的，windows是 \ linux和mac是 /
+# sep(["config", "environment.yaml"], True)
+# 输出 \config\environment.yaml
+def sep(path, add_sep_before=False, add_sep_after=False):
+    # 把数组合并成路径 ["config", "environment.yaml"] => config\environment.yaml
+    all_path = os.sep.join(path)
+    # 前面增加路径分隔符
+    if add_sep_before:
+        all_path = os.sep + all_path
+    # 后面面增加路径分隔符
+    if add_sep_after:
+        all_path = all_path + os.sep
+    return all_path
+
+if __name__ == '__main__':
+    # print(get_project_path())
+    print(sep(["config", "environment.yaml"], True))
---
+++ common/yaml_config.py
@@ -0,0 +1,19 @@
+# 需要安装 pyyaml
+import yaml
+# 引入工具函数
+from common.tools import get_project_path, sep
+
+
+class GetConf:
+    # GetConf() 相当于执行这个函数，读取environment.yaml配置文件的内容
+    def __init__(self):
+        # 找到environment.yaml文件并以读取的方式打开，encoding="utf-8"，utf-8编码格式，编程都是用这个格式打开文件，防止乱码
+        with open(get_project_path() + sep(["config", "environment.yaml"], add_sep_before=True), "r",
+                  encoding="utf-8") as env_file:
+            # 读取整个文件的内容赋值给 self.env ，一般是一个dict
+            self.env = yaml.load(env_file, Loader=yaml.FullLoader)
+            # print(self.env)
+
+
+if __name__ == '__main__':
+    print(GetConf())
